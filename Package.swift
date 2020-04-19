c02403705966011f81d3472eb2c1faa223cbd426
Mattt <mattt@me.com>
2020-04-19 15:54:49 -0700

    Update TAP dependency to 0.1.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-syntax.git", .revision("0.50200.0")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.4")),
-        .package(url: "https://github.com/SwiftDocOrg/TAP.git", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/SwiftDocOrg/TAP.git", .upToNextMinor(from: "0.1.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
