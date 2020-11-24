f9e0506e0fc2538e2e9bc453edbac1fe586009df
Mattt <mattt@me.com>
2020-11-24 03:47:31 -0800

    Update to swift-tools-version:5.3

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -13,24 +12,42 @@ let package = Package(
         .library(name: "DocTest", targets: ["DocTest"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-syntax.git", .revision("0.50300.0")),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.1")),
-        .package(url: "https://github.com/SwiftDocOrg/TAP.git", .upToNextMinor(from: "0.1.1")),
-        .package(url: "https://github.com/SwiftDocOrg/StringLocationConverter.git", .upToNextMinor(from: "0.0.1")),
-        .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.0")),
+        .package(name: "SwiftSyntax",
+                 url: "https://github.com/apple/swift-syntax.git",
+                 .revision("0.50300.0")
+        ),
+        .package(url: "https://github.com/apple/swift-argument-parser.git",
+                 .upToNextMinor(from: "0.3.1")
+        ),
+        .package(url: "https://github.com/SwiftDocOrg/TAP.git",
+                 .upToNextMinor(from: "0.1.1")
+        ),
+        .package(url: "https://github.com/SwiftDocOrg/StringLocationConverter.git",
+                 .upToNextMinor(from: "0.0.1")
+        ),
+        .package(url: "https://github.com/apple/swift-log.git",
+                 .upToNextMinor(from: "1.4.0")
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift-doctest",
-            dependencies: ["DocTest", "StringLocationConverter", "ArgumentParser", "Logging"]),
+            dependencies: [
+                .target(name: "DocTest"),
+                .product(name: "StringLocationConverter", package: "StringLocationConverter"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Logging", package: "swift-log"),
+            ]),
         .target(
             name: "DocTest",
-            dependencies: ["SwiftSyntax", "TAP"]),
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                .product(name: "TAP", package: "TAP"),
+            ]),
         .testTarget(
             name: "DocTestTests",
-            dependencies: ["DocTest"]),
+            dependencies: [
+                .target(name: "DocTest"),
+            ]),
     ]
 )
